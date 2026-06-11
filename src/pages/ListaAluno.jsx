bcda97fb44adaee4ddfffb107d18a78fbcca9ecc
Eduarda <euduardavitoria90@gmail.com>
2026-06-11 10:19:09 -0300

    Fix/correção na home e no arquivo de listar alunos que nao havia subido antes

--- src/pages/ListaAluno.jsx
+++ src/pages/ListaAluno.jsx
@@ -1,9 +1,47 @@
+
+import "./ListaAluno.css";
+import Header from "../components/Header";
+import { useNavigate } from "react-router-dom";
+
+
 function ListaAluno() {
+  const alunos = []; // futuramente virá do backend
+
+
   return (
-    <div>
-      <h1> Lista Aluno</h1>
-    </div>
+    <>
+      <Header />
+
+
+      <div className="lista-page">
+
+
+        <div className="lista-label">Lista de alunos</div>
+
+
+        <div className="lista-card">
+          <div className="lista-header-row">
+            <div className="lista-col lista-col--nome">Nome do aluno</div>
+            <div className="lista-col lista-col--id">Identificador</div>
+          </div>
+
+
+          <div className="lista-body">
+            {alunos.length === 0 ? null : alunos.map((aluno, i) => (
+              <div key={i} className="lista-row">
+                <div className="lista-col lista-col--nome">{aluno.nome}</div>
+                <div className="lista-col lista-col--id">{aluno.id}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+
+      </div>
+    </>
   );
 }
 
+
 export default ListaAluno;
+
